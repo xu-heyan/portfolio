cef68ac9a5027d46ca71e134d7f611c758ff8b93
ellyn <450661786@qq.com>
2020-11-26 20:10:22 +0800

    lazyload

--- index.js
+++ index.js
@@ -11,7 +11,31 @@ function setArctciles() {
 window.onresize = function () {
   setArctciles();
 };
+//懒加载
+var imgs = document.querySelectorAll("img");
+//offsetTop是元素与offsetParent的距离，循环获取直到页面顶部
+function getTop(e) {
+  var T = e.offsetTop;
+  while ((e = e.offsetParent)) {
+    T += e.offsetTop;
+  }
+  return T;
+}
 
+function lazyLoad(imgs) {
+  var H = document.documentElement.clientHeight; //获取可视区域高度
+  var S = document.documentElement.scrollTop || document.body.scrollTop;
+  for (var i = 0; i < imgs.length; i++) {
+    if (H + S > getTop(imgs[i])) {
+      imgs[i].src = imgs[i].getAttribute("data-src");
+    }
+  }
+}
+
+window.onload = window.onscroll = function () {
+  //onscroll()在滚动条滚动的时候触发
+  lazyLoad(imgs);
+};
 //平滑滚动到站内锚点
 let links = document.querySelectorAll(".about-me .bottom li");
 let anchorElements = document.querySelectorAll(".projects article");
